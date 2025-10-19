c1ad492c90b65df634c8f0a46444250aecf24c9f
srjorgedev <0324108148@ut-tijuana.edu.mx>
2025-10-18 19:00:34 -0700

    Agregando lista de opciones del menu principal

--- menus/mPrincipal.py
+++ menus/mPrincipal.py
@@ -1,8 +1,14 @@
 from interfaz import caja as UI
 
+opciones: list[str] = [
+    "Bitacoras", "Vehiculos", "Autorizar", "Solicitudes", "Usuarios",
+    "Mantenimiento", "Observaciones", "Salir"
+]
+
 # 0
 def _Principal():
-    UI._Titulo("SISTEMA DE GESTION DE ENTRADA, SALIDA Y MANTENIMIENTO DE VEHICULOS")
+    UI._Titulo(
+        "SISTEMA DE GESTION DE ENTRADA, SALIDA Y MANTENIMIENTO DE VEHICULOS")
     print("    1. Bitacoras")
     print("    2. Vehiculos")
     print("    3. Autorizar")
