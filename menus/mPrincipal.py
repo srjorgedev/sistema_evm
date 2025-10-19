de11e47b02f17777d6f38605660de3355b9fb35a
srjorgedev <0324108148@ut-tijuana.edu.mx>
2025-10-18 19:03:38 -0700

    Adaptando menu principal a las funciones de interfaz

--- menus/mPrincipal.py
+++ menus/mPrincipal.py
@@ -9,11 +9,5 @@ opciones: list[str] = [
 def _Principal():
     UI._Titulo(
         "SISTEMA DE GESTION DE ENTRADA, SALIDA Y MANTENIMIENTO DE VEHICULOS")
-    print("    1. Bitacoras")
-    print("    2. Vehiculos")
-    print("    3. Autorizar")
-    print("    4. Solicitudes")
-    print("    5. Usuarios")
-    print("    6. Mantenimiento")
-    print("    7. Observaciones")
-    print("    8. Salir\n")
+    UI._Lista(opciones)
+
